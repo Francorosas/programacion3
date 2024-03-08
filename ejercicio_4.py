398b05850221c9385dd5ff900bf460db5b7c203e
Francorosas <110985835+Francorosas@users.noreply.github.com>
2024-03-08 08:18:46 -0300

    Delete ejercicio_4.py

--- ejercicio_4.py
+++
@@ -1,7 +0,0 @@
-lista = []
-cant = int(input("Cuantos numero desea agregar a la lista: "))
-for i in range (cant):
-    valor = int(input("INGRESE EL NUMERO: "))
-    lista.append(valor)
-lista.sort()
-print("La lista ordenada seria la siguiente: ", lista)
