0d72800cea12132f14ea1a51a79259f0ea6b3dad
Francorosas <110985835+Francorosas@users.noreply.github.com>
2024-03-08 08:18:53 -0300

    Delete ejercicio_3.py

--- ejercicio_3.py
+++
@@ -1,9 +0,0 @@
-alto = int(input("Ingresar alto de un rectangulo: "))
-ancho = int(input("Ingresar ancho de un rectangulo: "))
-caracter = str(input("Ingrese un caracter: "))
-
-for i in range (alto):
-    for j in range (ancho):
-        print(caracter, end=" ")
-    print()
-    
